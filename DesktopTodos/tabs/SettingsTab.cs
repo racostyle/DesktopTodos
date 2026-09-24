738c88498e09a0f43388b76abff29479b67e3684
racostyle <cekcar@gmail.com>
2026-09-24 12:37:41 +0200

    tabs

---
+++ DesktopTodos/tabs/SettingsTab.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DesktopTodos.tabs
+{
+    public partial class SettingsTab : UserControl
+    {
+        public SettingsTab()
+        {
+            InitializeComponent();
+        }
+    }
+}
